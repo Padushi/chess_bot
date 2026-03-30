73a78a371d7d257734cdea44d177e547661ee806
Padushi <luca.aiden@gmail.com>
2026-03-30 14:19:02 -0400

    Added get_occupied_squares and renamed position to board for certain occurences

--- src/chess_game/board.py
+++ src/chess_game/board.py
@@ -1,5 +1,6 @@
-print("\n\033[95mboard.py is initialized \033\n[0m")
+print("\n\033[95mboard.py is initialized \033[0m")
 import pieces
+import board_utils
 
 class Board():
 
@@ -9,9 +10,9 @@ class Board():
 		'''
 
 		self.state = state
-		self.position = self.position_from_fen()
+		self.board = self.board_from_fen()
 
-	def position_from_fen(self):
+	def board_from_fen(self):
 
 		fen = self.state.split()
 		board = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],#h1
@@ -32,24 +33,24 @@ class Board():
 			elif i.lower() in 'rnbqkp':
 				board[row][col] = i
 				col += 1
+
 			if col == 8:
 				row += 1
 				col = 0
 		return board
 	
+	
+
 	def print_board_from_position(self):
 		for i in range(8):
 			print(f"+---+---+---+---+---+---+---+---+")
-			print(f"| {self.position[i][0]} | {self.position[i][1]} | {self.position[i][2]} | {self.position[i][3]} | {self.position[i][4]} | {self.position[i][5]} | {self.position[i][6]} | {self.position[i][7]} | {i+1}")
+			print(f"| {self.board[i][0]} | {self.board[i][1]} | {self.board[i][2]} | {self.board[i][3]} | {self.board[i][4]} | {self.board[i][5]} | {self.board[i][6]} | {self.board[i][7]} | {i+1}")
 		print(f"+---+---+---+---+---+---+---+---+")
 		print("  h   g   f   e   d   c   b   a")
 
 
-
-
-
 # default_board = Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')	
 
-# print(default_board.state)
 # default_board.print_board_from_position()
-			
+
+
--- src/chess_game/board_utils.py
+++ src/chess_game/board_utils.py
@@ -1,8 +1,3 @@
-import board
-import pieces
-
-
-
 def position_from_coordinates(rank: int, file: int) -> str:
 	return chr(104-rank) + str(file+1)
 
@@ -12,11 +7,22 @@ def coordinates_from_position(position: str) -> tuple[int,int]:
 	ex: a2 -> (7, 1)
 	ex: b1 -> (6, 0)
 	ex: f5 -> (2, 4)
-
-	a -> 7 -> 97
-	b -> 6 -> 98
-	c -> 5 -> 99
 	'''
 
-
 	return (ord('a') + 7 - ord(position[0]), int(position[1])-1)
+
+def get_occupied_squares(board):
+	'''
+	Loops through the board, adding the position of each cell occupied 
+	with a piece to a list, returns the list.
+	'''
+	
+	occupied_squares = []
+	for rank in board:
+		for file in rank:
+			if board[rank][file] != ' ':
+
+				occupied_squares.append(position_from_coordinates(rank, file))
+
+	return occupied_squares
+
