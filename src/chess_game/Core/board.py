9de3ecb54bd4251ab9fa31a93009295e7f19be4d
Padushi <luca.aiden@gmail.com>
2026-04-04 12:44:42 -0400

    Added functions for move generation

--- src/chess_game/Core/board.py
+++ src/chess_game/Core/board.py
@@ -11,41 +11,7 @@ class Board():
 		self.state = state
 		self.board = self.board_from_fen()
 
-	def board_from_fen(self):
 
-		fen = self.state.split()
-		board = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],#h1
-				[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], #g1 
-				[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], #f1 
-				[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], #e1 
-				[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], #d1 
-				[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], #c1 
-				[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], #b1 
-				[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']] #a1 a2 a3 a4 a5 a6 a7 a8
-		
-		col = 0
-		row = 0
-
-		for i in fen[0]:
-			if i.isdigit():
-				col += int(i)
-			elif i.lower() in 'rnbqkp':
-				board[row][col] = i
-				col += 1
-
-			if col == 8:
-				row += 1
-				col = 0
-		return board
-	
-	
-
-	def print_board_from_position(self):
-		for i in range(8):
-			print(f"+---+---+---+---+---+---+---+---+")
-			print(f"| {self.board[i][0]} | {self.board[i][1]} | {self.board[i][2]} | {self.board[i][3]} | {self.board[i][4]} | {self.board[i][5]} | {self.board[i][6]} | {self.board[i][7]} | {i+1}")
-		print(f"+---+---+---+---+---+---+---+---+")
-		print("  h   g   f   e   d   c   b   a")
 
 
 default_board = Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')	
--- src/chess_game/Core/move_generator.py
+++ src/chess_game/Core/move_generator.py
@@ -1,12 +1,30 @@
-import chess_game.Utility.board_utils
+from src.chess_game.Utility import board_utils
 
-
-def generate_orthogonal_moves(position, distance):
+def generate_diagonal_moves(position, distance):
 	'''
 	Takes in position in algebraic notation, distance is the max amount 
 	of tiles it can move, max distance of 8
 	'''
 	valid_files = 'abcdefgh'
+	diagonal_tiles = []
+	for i in range(1, distance+1):
+		diagonal_tiles.append(chr(ord(position[0]) + i) + str(int(position[1]) + i))
+		diagonal_tiles.append(chr(ord(position[0]) + i) + str(int(position[1]) - i))
+		diagonal_tiles.append(chr(ord(position[0]) - i) + str(int(position[1]) + i))
+		diagonal_tiles.append(chr(ord(position[0]) - i) + str(int(position[1]) - i))
+			
+	return list(filter(board_utils.position_in_board, diagonal_tiles))
+
+def generate_orthogonal_moves(position, distance):
+
 	orthogonal_tiles = []
 	for i in range(1, distance+1):
-		pass
+		orthogonal_tiles.append(chr(ord(position[0]) + i) + str(position[1]))
+		orthogonal_tiles.append(chr(ord(position[0]) - i) + str(position[1]))
+		orthogonal_tiles.append(position[0] + str((int(position[1]) + i)%9))
+		orthogonal_tiles.append(position[0] + str((int(position[1]) - i)%9))
+			
+	return list(filter(board_utils.position_in_board, orthogonal_tiles))
+	
+print(generate_diagonal_moves('d4', 1))
+print(generate_orthogonal_moves('d4', 5))
